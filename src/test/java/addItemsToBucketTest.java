948a9919ddda5685d516cc02442a3621a3442b2f
VladislavRb <vlad.raba@mail.ru>
2020-12-02 22:35:00 +0300

    implement first enabled input receival

--- src/test/java/addItemsToBucketTest.java
+++ src/test/java/addItemsToBucketTest.java
@@ -1,8 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 public class addItemsToBucketTest {
 
@@ -14,7 +25,27 @@ public class addItemsToBucketTest {
     }
 
     @Test
-    public void addSneakersToBucketListTest(){}
+    public void addSneakersToBucketListTest() {
+        driver.get("http://www.sportmaster.by/catalogitem/krossovki_mugskie_nike_md_runner_2749794n06010/");
+
+        Wait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(15))
+                .pollingEvery(Duration.ofSeconds(3))
+                .ignoring(NoSuchElementException.class)
+                .ignoring(StaleElementReferenceException.class)
+                .withMessage("Timeout for waiting search result list was exceeded!");
+
+        List<WebElement> sneakersSizes = wait
+                .until(ExpectedConditions
+                        .presenceOfAllElementsLocatedBy(By.xpath("//li[@class='cb-item-actions-data-sizes']//li")));
+
+        WebElement firstAvailableSize = sneakersSizes.stream()
+                .filter(webElement -> webElement.findElement(By.tagName("input")).isEnabled())
+                .findFirst()
+                .get();
+
+        Assert.assertEquals(firstAvailableSize.findElement(By.tagName("label")).getText(),"39,5");
+    }
 
     @AfterMethod (alwaysRun = true)
     public void browserQuit() {
